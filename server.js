0c9c90409873cd235f145157228114c6a65e5ef4
Abhishek Patra <patrasap0908@gmail.com>
2019-01-31 10:28:27 +0530

    Updated server file

--- server.js
+++ server.js
@@ -51,28 +51,18 @@ class Block{
 
 //Genesis Block Created 
 var BlockChain = new Array(100);
+
 var genDate = new Date();
-var genData = { 'index': 0, 'timeStamp': genDate, 'BPM': 0 };
-var genCipherText = CryptoJS.AES.encrypt(JSON.stringify(genData),'secret key 123');
-var b = new Block(genData,"",genCipherText.toString());
+var genData = { 
+                'index': 0, 
+                'timeStamp': genDate, 
+                'BPM': 0 
+            };
+
+var genCipherText = CryptoJS.AES.encrypt(JSON.stringify(genData), 'secret key 123');
+var b = new Block(genData, "", genCipherText.toString());
+
 BlockChain[0] = b;
-
-function createBlock(index,data){
-    var date = new Date;
-    var x = {'index':index,'timeStamp':date,'BPM':data};
-    var cipherText = CryptoJS.AES.encrypt(JSON.stringify(x), 'secret key 123');
-    var b = new Block(x,BlockChain[index-1].currHash,cipherText.toString());
-    BlockChain[index] = b;
-    console.log(BlockChain[index]);
-    count++;
-}
-
-function decryptBlock(index){
-    console.log("Decrypted:");
-    var bytes = CryptoJS.AES.decrypt(BlockChain[index].cipherText, 'secret key 123')
-    var plaintext = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-    console.log(plaintext);
-}
 
 
 // Login Route
@@ -106,29 +96,45 @@ app.post('/login',function(request, response, error) {
 
 
 //  Patient Routes
-app.post('/createPatientBlock', function (req, res, err) {
-    var encryptedText = crypto.encrypt(JSON.stringify(req.body));
+app.post('/createPatientBlock', function (request, response, error) {
+    var encryptedText = crypto.encrypt(JSON.stringify(request.body));
     // console.log("\nENCRYPTED TEXT: " + encryptedText);
     
-    var b = new Block(req.body, BlockChain[count-1].currHash, encryptedText);
+    var b = new Block(request.body, BlockChain[count-1].currHash, encryptedText);
     BlockChain[count] = b;
 
-    var sql = "INSERT INTO user(id, user_type, cipher_text, previous_hash, current_hash) VALUES ( " + count + ", '" +  (req.body.userType).toUpperCase() + "', '" + encryptedText + "', '" + b.prevHash + "', '" + b.currHash + "' );";
 
-    // console.log("\nINSERT QUERY: " + sql);
-    con.query(sql);
+    // Retrieve maximum ID from the database table
+    var getMaxId = "SELECT MAX(id) AS id FROM user;";
 
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
-        id: count
+    con.query(getMaxId, function(req, res, err) {
+        let userId;
+
+        if(res[0].id === null)
+            userId = 2019011;
+        else 
+            userId = res[0].id + 1;
+             
+        console.log(userId);        
+
+        response.setHeader('Content-Type', 'application/json');
+        response.send({
+            id: userId
+        });
+        response.end();
+
+
+        // Insert patient's details once ID has been established
+        var createPatient = "INSERT INTO user(id, user_type, cipher_text, previous_hash, current_hash) VALUES ( " + userId + ", '" +  (request.body.userType).toUpperCase() + "', '" + encryptedText + "', '" + b.prevHash + "', '" + b.currHash + "' );";
+
+        con.query(createPatient); 
     });
-    res.end();
-    
-    count++;
+        
+    count++; 
 });
 
 app.post('/displayPatientInfo', function(request, response, error) {
-    var sql = "SELECT cipher_text FROM user WHERE id = " + request.body.id; 
+    var sql = "SELECT id, cipher_text FROM user WHERE id = " + request.body.id; 
 
     con.query(sql, function(err, res) { 
         if(err)
@@ -143,7 +149,7 @@ app.post('/displayPatientInfo', function(request, response, error) {
     });
 });
 
-app.post('/getMyRequests', function(request, response, error) {
+app.post('/getPatientRequests', function(request, response, error) {
     var sql = "SELECT doctor_id FROM permission WHERE patient_id = " + request.body.id;
 
     con.query(sql, function(err, res) {
@@ -156,12 +162,32 @@ app.post('/getMyRequests', function(request, response, error) {
     });
 });
 
+app.post('/resolveRequest', function(request, response, error) {
+    if(request.body.access === true) {
+        con.query("INSERT INTO resolved_permission VALUES ( " + request.body.doctorId + ", " + request.body.patientId + ", 'YES' );");
+        
+        var removeRequest = "DELETE FROM permission WHERE patient_id = " + request.body.patientId + " AND doctor_id = " + request.body.doctorId;
+        con.query(removeRequest);
+
+        response.send("Peace");
+    }
+
+    else if(request.body.access === false) {
+        con.query("INSERT INTO resolved_permission VALUES ( " + request.body.doctorId + ", " + request.body.patientId + ", 'NO' );");
+        
+        var removeRequest = "DELETE FROM permission WHERE patient_id = " + request.body.patientId + " AND doctor_id = " + request.body.doctorId;
+        con.query(removeRequest);
+
+        response.send("Not Peace");
+    }
+});
+
 
 // Doctor Routes
 app.post('/searchForPatient', function (request, response, error) {
-    var sql = "SELECT id FROM user WHERE id = " + request.body.patientId + " AND user_type = 'PATIENT';";
+    var checkIfUserIsPresent = "SELECT id FROM user WHERE id = " + request.body.patientId + " AND user_type = 'PATIENT';";
 
-    con.query(sql, function (err, res) {
+    con.query(checkIfUserIsPresent, function(err, res) {
         if(err)
             console.error(err); 
 
@@ -176,11 +202,37 @@ app.post('/searchForPatient', function (request, response, error) {
                 message: "Request has been sent to Patient with ID " + request.body.patientId
             });
 
-            var sql1 = "INSERT INTO permission VALUES ( " + request.body.patientId + ", " + request.body.doctorId + " );";
+            var setPermissionRequest = "INSERT INTO permission VALUES ( " + request.body.patientId + ", " + request.body.doctorId + " );";
 
-            con.query(sql1);
+            con.query(setPermissionRequest);
         }
         response.end();
+    });
+});
+
+app.post('/getDoctorRequests', function(request, response, error) {
+    let output = {};
+
+    var sql1 = "SELECT patient_id AS patientId, access FROM resolved_permission WHERE doctor_id = " + request.body.id;
+
+    con.query(sql1, function(err, res) {
+        if(err)
+            console.error(err); 
+
+        output['resolvedPermissions'] = res;
+
+        var sql2 = "SELECT patient_id AS patientId FROM permission WHERE doctor_id = " + request.body.id;
+
+        con.query(sql2, function(err, res) {
+            if(err)
+                console.error(err);
+
+            output['pendingPermissions'] = res; 
+
+            response.setHeader('Content-Type', 'application/json');
+            response.send(output);
+            response.end();
+        });
     });
 });
 
